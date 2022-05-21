1ef5aa45f4a0411d48227226f5667b0a100c6eeb
Reza Roointan <m.roointan.10@gmail.com>
2022-05-21 20:04:40 +0430

    Add name field to appuser db model
    Add name property to appuser db model for store display name of user

--- src/Data/Entities/AppUser.cs
+++ src/Data/Entities/AppUser.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,7 @@ namespace Data.Entities
 {
     public class AppUser : IdentityUser
     {
+        [MaxLength(100)]
+        public string Name { get; set; }
     }
 }
---
+++ src/Data/Migrations/20220521153209_AddNameToAppUser.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    public partial class AddNameToAppUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "AspNetUsers",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "AspNetUsers");
+        }
+    }
+}
