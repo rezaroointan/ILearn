7f3ee4f5acdd86407c58cac319f47ecd64ae268f
Reza Roointan <m.roointan.10@gmail.com>
2022-05-23 21:19:18 +0430

    Add register
    Add register page for sign up user account

---
+++ src/Core/DTOs/Account/RegisterViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTOs.Account
+{
+    public class RegisterViewModel
+    {
+        [Display(Name = "ایمیل")]
+        [Required(ErrorMessage = "لطفا مقدار {0} را وارد کنید.")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Display(Name = "کلمه عبور")]
+        [Required(ErrorMessage = "لطفا مقدار {0} را وارد کنید.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "تکرار کلمه عبور")]
+        [Required(ErrorMessage = "لطفا مقدار {0} را وارد کنید.")]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage = "{0} و {1} با هم یکسان نیستند")]
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ src/Web/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using Core.DTOs.Account;
+using Data.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public AccountController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var name = model.Email.Split("@").First();
+
+                var user = new AppUser { Name = name, UserName = model.Email, Email = model.Email };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+
+            return View(model);
+        }
+    }
+}
