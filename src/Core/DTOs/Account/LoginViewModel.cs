32b726714c6dd05f8a01955dcbfecae84240b0cf
Reza Roointan <m.roointan.10@gmail.com>
2022-05-24 20:48:04 +0430

    Add login
    Add login in the user account

---
+++ src/Core/DTOs/Account/LoginViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTOs.Account
+{
+    public class LoginViewModel
+    {
+
+        [Display(Name = "ایمیل")]
+        [Required(ErrorMessage = "لطفا مقدار {0} را وارد کنید.")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Display(Name = "کلمه عبور")]
+        [Required(ErrorMessage = "لطفا مقدار {0} را وارد کنید.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "مرا به خاطر بسپار؟")]
+        public bool RememberMe { get; set; }
+    }
+}
--- src/Web/Controllers/AccountController.cs
+++ src/Web/Controllers/AccountController.cs
@@ -10,12 +10,14 @@ namespace Web.Controllers
     public class AccountController : Controller
     {
         private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
         private readonly IEmailSender _emailSender;
 
-        public AccountController(UserManager<AppUser> userManager, IEmailSender emailSender)
+        public AccountController(UserManager<AppUser> userManager, IEmailSender emailSender, SignInManager<AppUser> signInManager)
         {
             _userManager = userManager;
             _emailSender = emailSender;
+            _signInManager = signInManager;
         }
 
         [HttpGet]
@@ -74,6 +76,41 @@ namespace Web.Controllers
             var result = await _userManager.ConfirmEmailAsync(user, token);
 
             return Content(result.Succeeded ? "Email confirmed" : "Email not confirmed");
+        }
+
+        [HttpGet]
+        public IActionResult Login(string? returnUrl = null)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model, string? returnUrl = null)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, true);
+
+                if (result.Succeeded)
+                {
+                    return LocalRedirect(string.IsNullOrEmpty(returnUrl) ? Url.Content("~/") : returnUrl);
+                }
+
+                if (result.IsLockedOut)
+                {
+                    ModelState.AddModelError(string.Empty, "اکانت شما به دلیل پنج بار ورود ناموفق به مدت پنج دقیق قفل شده است");
+
+                    ViewBag.ReturnUrl = returnUrl;
+                    return View(model);
+                }
+
+                ModelState.AddModelError("", "رمزعبور یا نام کاربری اشتباه است");
+            }
+
+            ViewBag.ReturnUrl = returnUrl;
+            return View(model);
         }
     }
 }
